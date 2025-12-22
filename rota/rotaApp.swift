483d4ada311421074de1429f6ba85fe3fbc760bc
aslikocer00 <akocer017@gmail.com>
2025-12-22 10:53:59 +0300

    Initial Commit

---
+++ rota/rotaApp.swift
@@ -0,0 +1,17 @@
+//
+//  rotaApp.swift
+//  rota
+//
+//  Created by Asli Kocer on 22/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct rotaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
